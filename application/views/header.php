99ff0cfd4896f9bc53a200cc64468f9a40469788
Hoang Dung <dunghq87@gmail.com>
2015-06-23 06:59:02 +0700

    V 0.71

--- application/views/header.php
+++ application/views/header.php
@@ -27,17 +27,15 @@
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul class="nav navbar-nav text-uppercase">
 			      	<li><a href="/"> <i class="fa fa-home"></i> </a></li>
-			        <!--
+			        <li><a href="<?php echo base_url('/about')?>">Về chúng tôi Nagoya Việt Nam</a></li>
 			        <li class="dropdown">
-			        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Danh mục sản phẩm <span class="caret"></span></a>
+			        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Sản phẩm sản xuất<span class="caret"></span></a>
 				          <ul class="dropdown-menu text-capitalize" role="menu">
-				      		<?php	foreach ($categories as $key => $value) {
+				      		<?php	foreach ($cat_product as $key => $value) {
 			      				echo "<li><a href='".base_url().'danh-muc-san-pham/'.$value['slug']."'>".$value['name']."</a></li>";
 			      			}?>
 				          </ul>
 			        </li>
-			        -->
-			        <li><a href="<?php echo base_url('/about')?>">Về chúng tôi Nagoya Việt Nam</a></li>
 			        <?php	foreach ($cat_news as $key => $value) {
 	      				echo "<li><a href='".base_url().'danh-muc/'.$value['slug']."'>".$value['name']."</a></li>";
 	      			}?>
